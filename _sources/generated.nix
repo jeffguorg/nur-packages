2ff5d158379dc378caf8cf7f87172f5050f5405a
jeffguorg <jeffguorg@users.noreply.github.com>
2024-09-25 05:59:10 +0000

    auto: update packages

--- _sources/generated.nix
+++ _sources/generated.nix
@@ -3,15 +3,15 @@
 {
   cargo-create-tauri-app = {
     pname = "cargo-create-tauri-app";
-    version = "create-tauri-app-v4.4.0";
+    version = "create-tauri-app-v4.4.1";
     src = fetchFromGitHub {
       owner = "tauri-apps";
       repo = "create-tauri-app";
-      rev = "create-tauri-app-v4.4.0";
+      rev = "create-tauri-app-v4.4.1";
       fetchSubmodules = false;
-      sha256 = "sha256-uvPkwtK+IZSG0itFBjs3twffg5F17aqYNyKn2Vp1T08=";
+      sha256 = "sha256-CtALZvBtQBTLEvy7+UyTKUrRRNCj7qPQvzuGEiZUmZE=";
     };
-    "Cargo.lock" = builtins.readFile ./cargo-create-tauri-app-create-tauri-app-v4.4.0/Cargo.lock;
+    "Cargo.lock" = builtins.readFile ./cargo-create-tauri-app-create-tauri-app-v4.4.1/Cargo.lock;
   };
   kwok = {
     pname = "kwok";
